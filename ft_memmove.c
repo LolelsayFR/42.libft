8845358facfe8cbbc207c0500dfa97880503bb89
LolelsayFR <maillet.emilien@gmail.com>
2024-10-16 03:26:37 +0200

    fix memmove with exeption add

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:52:01 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/16 01:02:22 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/16 03:23:19 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	local_dest = dest;
 	local_temp = src;
+	if (!dest || !src)
+		return (NULL);
 	if (!(dest > src && dest < (src + n)))
 	{
 		i = 0;
@@ -32,11 +34,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	else
 	{
 		i = n;
-		while (i > 0)
-		{
+		while (i--)
 			local_dest[i] = local_temp[i];
-			i--;
-		}
 	}
 	return (dest);
 }
