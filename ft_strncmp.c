eb452eca13ff6a74de38afd0c6ce90c72823c1b5
LolelsayFR <maillet.emilien@gmail.com>
2024-10-17 01:23:28 +0200

    strcmp fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 09:23:53 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/17 01:04:49 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/17 01:22:55 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && (s1[i] || s2[i]))
 	{
-		if (!(s1[i] == s2[i]))
-			return (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
