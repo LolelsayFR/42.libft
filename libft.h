c8866d1a31ab9719c63a7189e1e846dd0d9da033
Lolelsay <maillet.emilien@gmail.com>
2024-10-17 01:59:54 +0200

    Update libft.h avec le garde fou

--- libft.h
+++ libft.h
@@ -9,9 +9,10 @@
 /*   Updated: 2024/10/16 20:56:46 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
-#include <stdlib.h>
+#ifndef LIBFT_H     
+# define LIBFT_H    
+# include <unistd.h>
+# include <stdlib.h>
 
 size_t	ft_strlen(const char *s);
 int		ft_atoi(char *str);
@@ -47,3 +48,5 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+#endif
