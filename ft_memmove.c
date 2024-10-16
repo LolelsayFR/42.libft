8660bb391586c40a061c7efdafbb01bea0dbe248
Emilien Maillet <emaillet@c1r2p13.42lehavre.fr>
2024-10-16 18:29:13 +0200

    fix ?

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:52:01 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/16 18:26:13 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/16 18:29:09 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	local_dest = dest;
 	local_temp = src;
-	if (!dest || !src)
+	if (dest == src || n == 0)
 		return (dest);
 	if (!(dest > src && dest < (src + n)))
 	{
