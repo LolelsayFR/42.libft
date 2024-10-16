9742da15e734fe85abffd65ab6353d399ba60b33
Emilien Maillet <emaillet@c1r1p11.42lehavre.fr>
2024-10-16 21:16:12 +0200

    strjoin add

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:52:01 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/16 21:10:59 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/16 21:16:02 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,21 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		count;
 	int		i;
 	char	*dest;
 
-	count = (ft_strlen(s1) + ft_strlen(s2) + 1);
-	dest = malloc(count * sizeof(char));
-	i = -1;
-	while (s1[i++] && count-- > 0)
+	dest = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	i = 0;
+	while (s1[i])
+	{
 		dest[i] = s1[i];
-	while (s2[i++] && count-- > 0)
+		i++;
+	}
+	while (s2[i])
+	{
 		dest[i] = s2[i];
-	i++;
+		i++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }
