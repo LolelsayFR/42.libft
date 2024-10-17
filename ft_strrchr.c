771249bf0b78e8576ecb154ddcdc946a0f2e00a2
Emilien Maillet <emaillet@c2r8p4.42lehavre.fr>
2024-10-18 01:06:31 +0200

    fix

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:50:52 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/17 18:03:39 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/18 01:06:25 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 
+	if (!s)
+		return (NULL);
 	i = ft_strlen(s);
 	while (s[i])
 	{
