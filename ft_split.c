dbb80ef3dc14e5fb36b790b6858dd4c29649ec99
LolelsayFR <maillet.emilien@gmail.com>
2024-10-22 16:55:54 +0200

    push SPLIT WAZA

--- ft_split.c
+++ ft_split.c
@@ -6,10 +6,77 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:52:01 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/21 17:02:21 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/22 16:54:18 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//char	**ft_split(char const *s, char c)
+static void	ft_freeall(char **result, size_t j)
+{
+	while (j--)
+		free(result[j]);
+	free(result);
+}
+
+static size_t	ft_countab(char const *s, char c)
+{
+	size_t	count;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i])
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+static char	**ft_splitinject(char const *s, char c, char **result)
+{
+	size_t	i;
+	size_t	j;
+	size_t	start;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i])
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			result[j] = ft_substr(s, start, i - start);
+			if (!result[j])
+				return (ft_freeall(result, j), NULL);
+			j++;
+		}
+	}
+	result[j] = NULL;
+	return (result);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+
+	if (!s)
+		return (NULL);
+	result = (char **)malloc((ft_countab(s, c) + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	return (ft_splitinject(s, c, result));
+}
