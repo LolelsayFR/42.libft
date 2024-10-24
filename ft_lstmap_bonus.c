0f4e7ddf34e4e49cb4511a9956eb7752e880bb75
LolelsayFR <maillet.emilien@gmail.com>
2024-10-24 16:28:58 +0200

    99%

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,10 +6,30 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 23:29:00 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/24 01:11:13 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/24 14:15:47 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*newfirst;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	newfirst = NULL;
+	while (lst)
+	{
+		new = ft_lstnew((*f)(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newfirst, new);
+		lst = lst->next;
+	}
+	return (newfirst);
+}
