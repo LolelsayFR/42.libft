be2acc94f0333b061eca43a2c6580e4a800daa37
Emilien Maillet <emaillet@c2r9p1.42lehavre.fr>
2024-10-25 20:29:04 +0200

    atoi without magic number

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:28:31 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/22 00:47:25 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/25 20:26:15 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_atoi(const char *str)
 	val = 0;
 	sign = 1;
 	i = 0;
-	while (str[i] == 32 || (str[i] > 8 && str[i] < 14))
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 23:28:20 by emaillet          #+#    #+#             */
-/*   Updated: 2024/10/24 00:05:28 by emaillet         ###   ########.fr       */
+/*   Updated: 2024/10/24 21:29:56 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*to_end;
 
-	if (!new)
-		return ;
 	if (!*lst)
 	{
 		*lst = new;
