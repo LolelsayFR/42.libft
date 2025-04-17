a7a0e9cbecebc5d86f413a3f5d744db22f1f4bf5
Lolelsay <maillet.emilien@gmail.com>
2025-04-17 09:20:06 +0200

    Update ft_strncmp.c

--- str/ft_strncmp.c
+++ str/ft_strncmp.c
@@ -14,26 +14,34 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
 
-	if (!s1 || !s2 || !n)
+	if ((!s1 && !s2) || n == 0)
 		return (0);
+	if (!s1)
+		return (s2[0]);
+	if (!s2)
+		return (s1[0]);
 	i = 0;
-	while (i < n && (s1[i] || s2[i]))
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (str1[i] && str1[i] == str2[i] && i < n - 1)
 		i++;
-	}
-	return (0);
+	return (str1[i] - str2[i]);
 }
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t	i;
 
-	if (!s1 || !s2)
-		return (0);
+	if (!s1)
+		return (s2[0]);
+	if (!s2)
+		return (s1[0]);
 	i = 0;
 	while (s1[i] || s2[i])
 	{
